bbf51d0a1344a4630fa5c1a48cf032f30e4426c5
Alexander Krestinskiy <incloudff@gmail.com>
2024-12-23 22:19:19 +0300

    feat: стили

--- src/features/github-issues/ui/github-Issues-list/githubIssuesList.tsx
+++ src/features/github-issues/ui/github-Issues-list/githubIssuesList.tsx
@@ -2,21 +2,24 @@ import { selectIssues } from 'entities/issues';
 import { FC } from 'react';
 import { useAppSelector } from 'shared/lib/store';
 import GithubIssueItem from '../github-issue-item/githubIssueItem';
+import styles from './githubIssuesList.module.scss';
 
 export const GithubIssuesList: FC = function GithubIssuesList() {
   const issues = useAppSelector(selectIssues);
 
   return (
-    <>
+    <div className={styles['issues-list']}>
       {issues.map((issue) => (
-        <GithubIssueItem
-          id={issue.number}
-          title={issue.title}
-          status={issue.state}
-          date={issue.created_at}
-          author={issue.user.login}
-        />
+        <div className='mb-5'>
+          <GithubIssueItem
+            id={issue.number}
+            title={issue.title}
+            status={issue.state}
+            date={issue.created_at}
+            author={issue.user.login}
+          />
+        </div>
       ))}
-    </>
+    </div>
   );
 };
